c06619a16d1fc16cc04bc1897fa273377ac5ea6f
t3ffsung <mrsecret11118@gmail.com>
2026-03-01 14:20:57 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,25 @@
-const CACHE_NAME = "tic-tac-toe-v1";
+const CACHE_NAME = "tic-tac-toe-v2";
+
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
   );
 });
